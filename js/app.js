28397c3e7a722d0ce48a0bec42d096556e22fa2c
Simon X Han <han.simonx@gmail.com>
2017-03-11 16:47:53 -0800

    custom styling

--- js/app.js
+++ js/app.js
@@ -1,5 +1,106 @@
 'use strict';
 
+// Custom Google Maps Style
+
+var mapStyle = [
+    {
+        "featureType": "administrative",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "on"
+            },
+            {
+                "lightness": 33
+            }
+        ]
+    },
+    {
+        "featureType": "landscape",
+        "elementType": "all",
+        "stylers": [
+            {
+                "color": "#f2e5d4"
+            }
+        ]
+    },
+    {
+        "featureType": "poi.park",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#c5dac6"
+            }
+        ]
+    },
+    {
+        "featureType": "poi.park",
+        "elementType": "labels",
+        "stylers": [
+            {
+                "visibility": "on"
+            },
+            {
+                "lightness": 20
+            }
+        ]
+    },
+    {
+        "featureType": "road",
+        "elementType": "all",
+        "stylers": [
+            {
+                "lightness": 20
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#c5c6c6"
+            }
+        ]
+    },
+    {
+        "featureType": "road.arterial",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#e4d7c6"
+            }
+        ]
+    },
+    {
+        "featureType": "road.local",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#fbfaf7"
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "on"
+            },
+            {
+                "color": "#acbcc9"
+            }
+        ]
+    }
+];
+
+/*
+ *
+ * Main app
+ *
+ */
+
 (function($) {
     $(function() {
 
@@ -156,11 +257,14 @@
                         lat: 37.7749,
                         lng: -122.4194
                     },
-                    mapTypeId: google.maps.MapTypeId.ROADMAP,
+                    zoomControl: true,
+                    streetViewControl: true,
+                    // mapTypeId: google.maps.MapTypeId.ROADMAP,
+                    styles: mapStyle,
                     mapTypeControlOptions: {
                         style: google.maps.MapTypeControlStyle.DROPDOWN_MENU,
                     },
-                    disableDefaultUI: false
+                    disableDefaultUI: true
                 });
                 createFilterButton();
                 createGeolocationButton();
